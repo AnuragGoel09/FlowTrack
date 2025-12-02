49c6607c797c4bb37acea33bbaefb192bfad0c09
AnuragGoel09 <anurag.goel.9303@gmail.com>
2025-12-02 20:41:17 +0530

    removed build

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_flowTrack_FILE D:/workdir/FlowTrack/FlowTrackDesktop/build/flowTrack.exe)
-set(__QT_DEPLOY_TARGET_flowTrack_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_flowTrack_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
